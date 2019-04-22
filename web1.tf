a74747f99a70ebde81fe151e518daf65055badb1
nlevo <nicolas.levo@gmail.com>
2019-04-22 19:33:33 -0400

    Add 2 droplets

---
+++ web1.tf
@@ -0,0 +1,18 @@
+resource "digitalocean_droplet" "web1" {
+  image = "ubuntu-18-04-x64"
+  name = "web1"
+  region = "nyc1"
+  size = "512mb"
+  private_networking = true
+  user_data = "${file("config/webuserdata.sh")}"
+  ssh_keys = [
+      "${var.ssh_fingerprint}"
+  ]
+  connection {
+    agent = true
+    user = "root"
+    type = "ssh"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
+  }
+}
